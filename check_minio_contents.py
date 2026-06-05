fbbc1772445054157f6290d8c981051ebd6fb773
Mperagon <miguelperagonc@gmail.com>
2026-06-05 23:31:41 +0200

    Remove test and obsolete scripts

--- check_minio_contents.py
+++
@@ -1,6 +0,0 @@
-from minio import Minio
-c = Minio('127.0.0.1:9000', access_key='minioadmin', secret_key='minioadmin', secure=False)
-objs = list(c.list_objects('flight-data', recursive=True))
-print(f'Objetos en MinIO: {len(objs)}')
-for o in objs:
-    print(f'  {o.object_name}  ({o.size/1024/1024:.2f} MB)')
